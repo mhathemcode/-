9e655e1b65234d66c3dae9a6035472808cc5cb87
snake.x <mohamed.hathem.code@gmail.com>
2025-09-24 20:35:45 +0300

    Update حساب الزمن.py
    تم تحسين الكود و جعله اكثر وضوحا

--- حساب الزمن.py
+++ حساب الزمن.py
@@ -1,11 +1,5 @@
-# مشروع حساب الثواني إلى ساعات ودقايق وثواني
 t_seconds =int(input('please enter number seconds\n'))
-
-hours = t_seconds // 3600             
-elbage= t_seconds % 3600           
-minutes = elbage// 60                  
-seconds = elbage% 60   
-           
-print(f'{hours} hours')
-print(f'{minutes} minutes')
-print(f'{seconds} seconds')
+hours =t_seconds // 3600                     
+minutes =(t_seconds%3600)/ 60                  
+seconds = t_seconds % 60              
+print(f'{hours} hours\n{minutes} minutes\n({seconds} seconds')
