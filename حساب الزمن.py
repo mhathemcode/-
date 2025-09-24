a585d13dbe4547a0a11ba4ad89dcc42ada458b16
snake.x <mohamed.hathem.code@gmail.com>
2025-09-24 20:40:38 +0300

    Update حساب الزمن.py
    الكود النهائي

--- حساب الزمن.py
+++ حساب الزمن.py
@@ -2,5 +2,4 @@ t_seconds =int(input('please enter number seconds\n'))
 hours =t_seconds // 3600                     
 minutes =(t_seconds%3600)//60                  
 seconds = t_seconds % 60              
-print(f'{hours} hours\n{minutes} minutes\n(
-{seconds} seconds')
+print(f'{hours} hours\n{minutes} minutes\n {seconds} seconds')
